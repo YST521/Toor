951a5377a129d78f5abe7386d4c18d1e97606e11
youxin <youxin@youxins-Mac-mini.local>
2017-07-10 17:34:21 +0800

    cell优化监听tabview滑动高度

--- DisplayTestDemo-（cell优化方法集合）/DisplayLayout/DisplayLayout/ViewController/YYKitTableViewController/NextViewController.m
+++ DisplayTestDemo-（cell优化方法集合）/DisplayLayout/DisplayLayout/ViewController/YYKitTableViewController/NextViewController.m
@@ -14,6 +14,13 @@
 @interface NextViewController ()<UITableViewDelegate,UITableViewDataSource>
 @property(nonatomic,strong)UITableView *tabview;
 @property (nonatomic, strong) DataSupport *dataSupport;
+@property(nonatomic,strong)UIView*headView;
+@property(nonatomic,strong)UIView *hview;
+
+// 监测范围的临界点,>0代表向上滑动多少距离,<0则是向下滑动多少距离
+@property (nonatomic, assign)CGFloat threshold;
+// 记录scrollView.contentInset.top
+@property (nonatomic, assign)CGFloat marginTop;
 
 @end
 
@@ -50,10 +57,75 @@
     self.tabview.dataSource = self;
     self.tabview.delegate  = self;
     [self.tabview registerClass:[NextTableViewCell class] forCellReuseIdentifier:@"CELL"];
+    self.headView = [[UIView alloc]init];
+    self.headView.frame = CGRectMake(0, 0, kScreenWidth, 200);
+    self.headView.backgroundColor =[UIColor greenColor];
+     self.tabview.tableHeaderView = self.headView;
+    
+    self.hview =[[UIView alloc]init];
+    self.hview.frame = CGRectMake(kScreenWidth/2-50, 50, 100, 100);
+    self.hview.backgroundColor =[UIColor yellowColor];
+    [ self.tabview addSubview:self.hview];
+    
+    // 这里设值-80，即向下滑动80，-80到0就是咱们重点监测的范围
+    self.threshold = -80;
     
 
+    
+//监听tabview滑动xy    http://www.cnblogs.com/sunfuyou/p/6245145.html
+//
+//    if (self.tabview.screenViewY <kScreenHeight*1.5) {
+//         self.tabview.tableHeaderView = self.headView;
+//    } else {
+//          [self.view addSubview:self.headView];
+//    }
+
+
+    
 
 }
+
+-(void)aa{
+
+}
+
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    
+    if (self.marginTop != scrollView.contentInset.top) {
+        self.marginTop = scrollView.contentInset.top;
+    }
+    
+    CGFloat offsetY = scrollView.contentOffset.y;
+    CGFloat newoffsetY = offsetY + self.marginTop;
+    
+    // 临界值150，向上拖动时变透明
+    if (newoffsetY >= 5 && newoffsetY <= 50) {
+
+        [UIView animateWithDuration:3.0 animations:^{
+            self.hview.frame =self.headView.bounds;
+            self.hview.backgroundColor =[UIColor yellowColor];
+            self.title = @"666";
+            
+        }];
+        
+  
+    }else if (newoffsetY > 50){
+
+          [UIView animateWithDuration:3.0 animations:^{
+             self.hview.frame =self.headView.bounds;
+                self.hview.backgroundColor =[UIColor purpleColor];
+                self.title = @"888";
+               }];
+    }else{
+
+        
+        self.hview.frame = CGRectMake(kScreenWidth/2-50, 50, 100, 100);
+        self.hview.backgroundColor =[UIColor cyanColor];
+    }
+    
+}
+
 
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     
@@ -64,11 +136,29 @@
     }
     return 100;
 }
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
 
     return self.dataSource.count;
 }
+
+-(UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section{
+    UIView *view =[[UIView alloc]init];
+    view.backgroundColor =[UIColor redColor];
+    view.frame = CGRectMake(0, 0, kScreenWidth, 40);
+    
+ 
+    
+    return view;
+
+}
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+   return  @"666";
+}
+
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     NextTableViewCell *cell =[self.tabview dequeueReusableCellWithIdentifier:@"CELL"];
